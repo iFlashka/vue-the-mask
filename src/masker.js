64c0482449952138394e42476fb2b4adc5b16436
seydullaev.aslan <aslan10@mail.ru>
2019-08-12 06:00:39 +0300

    changes

--- src/masker.js
+++ src/masker.js
@@ -1,7 +1,9 @@
 import maskit from './maskit'
 import dynamicMask from './dynamic-mask'
+import defaultTokens from './tokens'
+
 // Facade to maskit/dynamicMask when mask is String or Array
-export default function (value, mask, masked = true, tokens) {
+export default function (value, mask, masked = true, tokens = defaultTokens) {
   return Array.isArray(mask)
          ? dynamicMask(maskit, mask, tokens)(value, mask, masked, tokens)
          : maskit(value, mask, masked, tokens)
